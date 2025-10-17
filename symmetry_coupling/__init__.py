aceff5625ee4767a454d1e00b46ace506b8b0782
Abraham Rubinestock <abbrubin150@gmail.com>
2025-10-17 23:53:12 +0300

    Add pure Python OnTheEdgeLearning module

--- symmetry_coupling/__init__.py
+++ symmetry_coupling/__init__.py
@@ -1,12 +1,7 @@
 """Symmetry Coupling Metric package."""
 
-from .core import (
-    Config,
-    Metrics,
-    compute_coupling,
-    compute_coupling_batch,
-    load_synonym_dictionary,
-)
+from .core import Config, Metrics, compute_coupling, compute_coupling_batch, load_synonym_dictionary
+from .on_the_edge_learning import OnTheEdgeLearning
 
 __all__ = [
     "Config",
@@ -14,4 +9,5 @@ __all__ = [
     "compute_coupling",
     "compute_coupling_batch",
     "load_synonym_dictionary",
+    "OnTheEdgeLearning",
 ]
